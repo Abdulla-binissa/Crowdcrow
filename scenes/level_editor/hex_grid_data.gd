902a2cd106a4a2da2a33592a851d1adc4ab9460a
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-28 11:54:11 -0700

    Add HexGridData resource
    - Grid data resource contains grid size and grid_dict
    - grid_dict uses Vector2 as key, which correspond to the coordinate of
    the tile
    - Using resource inorder to save grid_dict for different levels

---
+++ scenes/level_editor/hex_grid_data.gd
@@ -0,0 +1,14 @@
+class_name  HexGridData
+extends Resource
+
+@export_range (2, 20, 1) var grid_size := 16
+var grid_dict: Dictionary = {}
+
+func _ready():
+	_generate_grid_dictionary()
+
+func _generate_grid_dictionary():
+	grid_dict.clear()
+	for y in range(grid_size):
+		for x in range(grid_size):
+			grid_dict[Vector2(x, y)] = {"id": 1}
