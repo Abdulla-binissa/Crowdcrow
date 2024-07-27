80885ffbe615ed61e7d49d32e33f9c65f9c6c21b
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-07-26 18:48:09 -0700

    Rename level_editor_menu to select_level_menu

--- scripts/ui/main_menu/main_menu.gd
+++ scripts/ui/main_menu/main_menu.gd
@@ -7,7 +7,7 @@ func _on_play_button_pressed():
 
 
 func _on_level_editor_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/menu/level_editor_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/menu/select_level_menu.tscn")
 
 
 func _on_exit_button_pressed():
